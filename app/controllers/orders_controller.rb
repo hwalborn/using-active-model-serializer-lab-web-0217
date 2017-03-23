137b741cba339a62fba63cbc5bc119e629ce5f13
hwalborn <hwalborn@gmail.com>
2017-03-23 17:06:43 -0400

    Booyakasha

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,11 @@ class OrdersController < ApplicationController
     @orders = Order.all
   end
 
+  def show
+    @author = Author.find(params[:id])
+    respond_to do |f|
+      f.html {render :show}
+      f.json {render json: @author}
+    end
+  end
 end
